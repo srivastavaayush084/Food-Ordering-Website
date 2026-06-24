b7a134c2c6acd9a6141b2a225e8f6f6833888c63
srivastavaayush084 <srivastavaayush084@gmail.com>
2026-06-24 11:41:07 +0530

    Initial commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import "./Footer.css";
+import { assets } from "../../assets/assets";
+import { useNavigate } from "react-router-dom";
+
+const Footer = () => {
+  const navigate = useNavigate();
+  return (
+    <footer id="footer">
+      <div className="footer-content">
+        <div className="footer-content-left">
+          <img src={assets.logo} alt="logo" className="footer-logo"/>
+          <p>
+            "Desi Flavours,Delivered Fresh."
+            <br />
+            From our kitchen to your heart...
+          </p>
+          <div className="footer-social-icons">
+            <a href="https://www.facebook.com">
+              <img src={assets.facebook_icon} alt="facebook" />
+            </a>
+            <a href="https://www.twitter.com">
+              <img src={assets.twitter_icon} alt="twitter" />
+            </a>
+            <a href="https://www.linkedin.com">
+              <img src={assets.linkedin_icon} alt="linkedin" />
+            </a>
+          </div>
+        </div>
+        <div className="footer-content-center">
+          <h2>COMPANY</h2>
+          <ul>
+            <li onClick={() => navigate("/")}>Home</li>
+            <li>About Us</li>
+            <li>Delivery</li>
+            <li>Privacy Policy</li>
+          </ul>
+        </div>
+        <div className="footer-content-right">
+          <h2>GET IN TOUCH</h2>
+          <ul>
+            <li>+91-9454175864</li>
+            <li>contact@desibites.com</li>
+          </ul>
+        </div>
+      </div>
+      <hr />
+      <p className="footer-copyright">Copyright 2024 © desibites.com</p>
+    </footer>
+  );
+};
+
+export default Footer;
